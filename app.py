abf47af5c6ec29dd5143bef97c542247c8523bff
Nathaniel Warner <nathanielwarner77@gmail.com>
2019-04-13 16:05:03 -0500

    Added dashboard

--- app.py
+++ app.py
@@ -12,23 +12,42 @@ class Database:
         result = self.db.orgs.find()
         return result
 
-    def get_one_org(self, org_id):
-        print("todo")
+    def get_org(self, acronym):
+        result = self.db.orgs.find({"acronym": acronym})
+        return result
 
     def insert_org(self, data):
         return self.db.orgs.insert_one(data.to_dict()).inserted_id
 
+    def update_org(self, data):
+        org_data = data.to_dict()
+        return self.db.orgs.replace_one({"acronym": org_data["acronym"]}, org_data)
+
 
 @app.route('/')
+def index():
+    return "Hello World!"
+
+
+@app.route('/master_admin')
 def all_orgs():
     db = Database()
     res = db.list_all_orgs()
     return render_template('all_orgs.html', result=res, content_type='application/json')
 
 
-@app.route('/org/<path:text>')
-def one_org(text):
+@app.route('/org/details/<path:text>')
+def org_details(text):
     db = Database()
+    res = db.get_org(text)
+    return render_template('org_details.html', result=res[0], content_type='application/json')
+
+
+@app.route('/org/dashboard/<path:text>')
+def org_dashboard(text):
+    db = Database()
+    res = db.get_org(text)
+    return render_template('org_dashboard.html', result=res[0], content_type='application/json')
 
 
 @app.route('/insert', methods=['POST'])
@@ -38,6 +57,16 @@ def insert_data():
     response = db.insert_org(form_data)
     print(response)
     return redirect('/')
+
+
+@app.route('/update', methods=['POST'])
+def update_data():
+    form_data = request.form
+    db = Database()
+    response = db.update_org(form_data)
+    print(response)
+    return redirect('/')
+
 
 if __name__ == '__main__':
     app.run()
