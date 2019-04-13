93894d7833685a591c83c25b5167ad509ebfe4d2
Nathaniel Warner <nathanielwarner77@gmail.com>
2019-04-13 15:07:29 -0500

    Switch to mongodb

--- app.py
+++ app.py
@@ -1,31 +1,43 @@
-from flask import Flask, render_template
-import pymysql
+from flask import Flask, render_template, request, redirect
+from pymongo import MongoClient
 app = Flask(__name__)
 
 
 class Database:
     def __init__(self):
-        host = "127.0.0.1"
-        user = "user"
-        password = "password"
-        db = "student_orgs"
-        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.
-                                   DictCursor)
-        self.cur = self.con.cursor()
+        client = MongoClient('localhost', 27017)
+        self.db = client.student_orgs
 
     def list_all_orgs(self):
-        self.cur.execute("SELECT org_id, org_name FROM orgs LIMIT 50")
-        result = self.cur.fetchall()
+        result = self.db.orgs.find()
         return result
+
+    def get_one_org(self, org_id):
+        print("todo")
+
+    def insert_org(self, data):
+        return self.db.orgs.insert_one(data.to_dict()).inserted_id
 
 
 @app.route('/')
 def all_orgs():
     db = Database()
     res = db.list_all_orgs()
-    print(res)
     return render_template('all_orgs.html', result=res, content_type='application/json')
 
+
+@app.route('/org/<path:text>')
+def one_org(text):
+    db = Database()
+
+
+@app.route('/insert', methods=['POST'])
+def insert_data():
+    form_data = request.form
+    db = Database()
+    response = db.insert_org(form_data)
+    print(response)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
